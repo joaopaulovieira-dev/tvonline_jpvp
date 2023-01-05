094afe58f8848afea1728670543f450bc8c8bbae
João Paulo Vieira <contato@joaopaulovieira.dev>
2023-01-05 12:15:35 -0300

    Alterando o menu dos cards.

--- lib/business_logic/provider/channelCardprovider.dart
+++ lib/business_logic/provider/channelCardprovider.dart
@@ -4,61 +4,73 @@ import 'package:tvonline_jpvp/business_logic/model/channel_model.dart';
 final channelCardProvider = StateProvider<List<ChannelModel>>((ref) {
   List<ChannelModel> channelsCard = [
     ChannelModel(
-        name: "Live Tv", iconAddress: "assets/icons/tv.svg", channelCount: 0),
+        name: "Live TV", iconAddress: "assets/icons/tv.svg", channelCount: 0),
     ChannelModel(
-        name: "Movies",
+        name: "TV Aberta", iconAddress: "assets/icons/tv.svg", channelCount: 0),
+    ChannelModel(
+        name: "Esporte",
+        iconAddress: "assets/icons/sport.svg",
+        channelCount: 0),
+    ChannelModel(
+        name: "Notícia", iconAddress: "assets/icons/news.svg", channelCount: 0),
+    ChannelModel(
+        name: "Gospel", iconAddress: "assets/icons/news.svg", channelCount: 0),
+    ChannelModel(
+        name: "Série",
         iconAddress: "assets/icons/popcorn.svg",
         channelCount: 0),
     ChannelModel(
-        name: "Series", iconAddress: "assets/icons/movie.svg", channelCount: 0),
+        name: "Filme", iconAddress: "assets/icons/movie.svg", channelCount: 0),
     ChannelModel(
-        name: "Animation",
+        name: "Desenho",
         iconAddress: "assets/icons/animation.svg",
         channelCount: 0),
     ChannelModel(
-        name: "Music", iconAddress: "assets/icons/music.svg", channelCount: 0),
+        name: "Anime",
+        iconAddress: "assets/icons/animation.svg",
+        channelCount: 0),
     ChannelModel(
-        name: "Auto", iconAddress: "assets/icons/auto.svg", channelCount: 0),
+        name: "Música", iconAddress: "assets/icons/music.svg", channelCount: 0),
     ChannelModel(
-        name: "Sport", iconAddress: "assets/icons/sport.svg", channelCount: 0),
+        name: "Carro", iconAddress: "assets/icons/auto.svg", channelCount: 0),
     ChannelModel(
-        name: "News", iconAddress: "assets/icons/news.svg", channelCount: 0),
-    ChannelModel(
-        name: "Coocking",
+        name: "Cozinha",
         iconAddress: "assets/icons/coocking.svg",
         channelCount: 0),
     ChannelModel(
-        name: "Kids", iconAddress: "assets/icons/kids.svg", channelCount: 0),
+        name: "Infantil",
+        iconAddress: "assets/icons/kids.svg",
+        channelCount: 0),
     ChannelModel(
-        name: "Education",
+        name: "Educação",
         iconAddress: "assets/icons/education.svg",
         channelCount: 0),
     ChannelModel(
-        name: "Business",
+        name: "Negócio",
         iconAddress: "assets/icons/business.svg",
         channelCount: 0),
     ChannelModel(
-        name: "Relaxation",
+        name: "Relaxamento",
         iconAddress: "assets/icons/relaxation.svg",
         channelCount: 0),
     ChannelModel(
-        name: "Entertainment",
+        name: "Entretenimento",
         iconAddress: "assets/icons/entertainment.svg",
         channelCount: 0),
     ChannelModel(
-        name: "LifeStyle",
+        name: "Estilo de Vida",
         iconAddress: "assets/icons/lifeStyle.svg",
         channelCount: 0),
     ChannelModel(
-        name: "Science",
+        name: "Ciência",
         iconAddress: "assets/icons/science.svg",
         channelCount: 0),
     ChannelModel(
-        name: "Comedy",
+        name: "Comédia",
         iconAddress: "assets/icons/comedy.svg",
         channelCount: 0),
     ChannelModel(
-        name: "Family",
+        name: "Família",
         iconAddress: "assets/icons/family.svg",
         channelCount: 0),
     ChannelModel(
@@ -66,3 +78,6 @@ final channelCardProvider = StateProvider<List<ChannelModel>>((ref) {
   ];
   return channelsCard;
 });
+
+
+//Aqui fica os estilos de canais nos card do menu principal (HomePage).
